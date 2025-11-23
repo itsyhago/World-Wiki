55daa668ca61427db499da4cad11ee5cb5f85bfa
Yhago Pessoa <yhagopessoad@gmail.com>
2025-11-23 17:33:58 -0300

    map update

---
+++ js/index.js
@@ -0,0 +1,123 @@
+history.scrollRestoration = "manual";
+
+const guideTitle = document.querySelectorAll(".guideTitle")  /* List of divs with the names of the continents. */
+const topBtn = document.getElementById("topBtn")  /* back to top button */
+const ulMenu = document.querySelector(".ulMenu")  /* hamburger menu */
+const inputfield =  document.querySelector("input")  /* Input */
+const container =  document.querySelector(".suggestionsContainer")   /* suggestionsContainer */
+const dataList = [
+"Africa do Sul", "Angola", "Argelia", "Benim", "Botsuana", "Burkina Fasso", "Burundi", "Cabo Verde"
+
+]
+const dataListLower = dataList.map(item => item.toLowerCase())
+
+let position  = Array.from(guideTitle).map(el => el.offsetTop);/* list of guide title positions */
+let timer /* timer when resizing the page */
+
+
+function removeAccent(str){
+         return str
+                    .normalize("NFD")
+                    .replace(/[\u0300-\u036f]/g, "")
+}
+document.querySelector(".menu").addEventListener("click", ()=>{
+    ulMenu.classList.toggle("aparecer")
+    setTimeout(() =>{
+        position = Array.from(guideTitle).map(el => el.offsetTop);
+        localStorage.setItem("position", JSON.stringify(position));
+    }, 500)
+    
+})
+
+
+window.addEventListener('resize', () =>{
+    clearTimeout(timer)
+    timer = setTimeout(() => {
+        localStorage.removeItem("position")
+        location.href = "index.html"
+    }, 250)
+})
+window.addEventListener("scroll", () => {
+    guideTitle.forEach((div, i) =>{
+        let positionsInv = Array.from(guideTitle).map(el => el.offsetTop);
+        const nextDiv = guideTitle[i + 1]
+        if (window.scrollY >= position[i] && (!nextDiv || window.scrollY < position[i + 1]  - 100)){
+            div.classList.add("grudada")
+        }else{
+            div.classList.remove("grudada")
+           
+        }
+        if(positionsInv[i] >= positionsInv[i + 1]){
+            div.classList.add("invisivel")
+        }else{
+            div.classList.remove("invisivel")
+        }
+        console.log(window.scrollY)
+        console.log(position)
+        if ((window.innerHeight + window.scrollY + 3 >= document.body.offsetHeight) && !nextDiv) {
+            if( window.scrollY >= position[i]){
+                div.classList.add("invisivel") 
+            }                
+        }
+    })
+
+    if (window.scrollY >= 1000){
+        topBtn.classList.add("aparecer")
+    }else{
+        topBtn.classList.remove("aparecer")
+    }
+
+    
+})
+
+inputfield.addEventListener("input", function() {
+    const userInput = this.value.toLowerCase();
+    container.innerHTML = ""
+    const filteredSuggestions = dataList.filter(item => item.toLowerCase().startsWith(userInput))
+    if(userInput === ""){
+       container.style.display = "none"; 
+        return
+    }
+
+    if(filteredSuggestions.length === 0){
+        container.style.display = "none"; 
+        return
+    }
+
+    container.style.display = "block"
+   filteredSuggestions.forEach(suggestion =>{
+        const itemDiv = document.createElement("div")
+        itemDiv.classList.add("suggestions-item")
+        itemDiv.textContent = suggestion
+
+        itemDiv.addEventListener("click", () =>{
+            inputfield.value = suggestion
+            container.innerHTML = ""
+            container.style.display = "none"
+            inputfield.focus()
+        })
+         container.appendChild(itemDiv);
+   })
+
+    
+})
+
+document.addEventListener("keydown", function(event){
+    const key = event.key
+    if(document.activeElement === inputfield && key == "Enter"){
+        if(dataListLower.includes(inputfield.value.toLowerCase()) ){
+            window.open(`pages/${removeAccent(inputfield.value).toLowerCase().replaceAll(" ", "")}.html`, "_self")
+            inputfield.classList.remove("error")
+            inputfield.value = ""
+        }else{
+           inputfield.classList.add("error")
+           inputfield.placeholder = "Não reconhecido"
+           inputfield.value = ""
+            container.innerHTML = ""
+            container.style.display = "none"
+        }
+        
+    }
+
+
+})
---
+++ js/mapa.js
@@ -0,0 +1,83 @@
+function removeAccent(str){
+         return str
+                    .normalize("NFD")
+                    .replace(/[\u0300-\u036f]/g, "")
+}
+
+am5.ready(function() {
+
+// Create root element
+// https://www.amcharts.com/docs/v5/getting-started/#Root_element
+var root = am5.Root.new("chartdiv");
+
+
+// Set themes
+// https://www.amcharts.com/docs/v5/concepts/themes/
+root.setThemes([
+  am5themes_Animated.new(root)
+]);
+
+
+// Create the map chart
+// https://www.amcharts.com/docs/v5/charts/map-chart/
+var chart = root.container.children.push(am5map.MapChart.new(root, {
+  panX: "translateX",
+  panY: "translateY",
+  projection: am5map.geoMercator()
+}));
+
+
+// Create main polygon series for countries
+// https://www.amcharts.com/docs/v5/charts/map-chart/map-polygon-series/
+var polygonSeries = chart.series.push(am5map.MapPolygonSeries.new(root, {
+  geoJSON: am5geodata_worldLow,
+  exclude: ["AQ"],
+
+}));
+
+    polygonSeries.mapPolygons.template.adapters.add("tooltipText", function(tooltipText, target) {
+        var dataContext = target.dataItem.dataContext;
+        var countryId = dataContext.id;
+        
+        // Tentar obter nome em português
+        var nomePortugues = am5geodata_lang_PT[countryId];
+        
+        if (nomePortugues) {
+            return nomePortugues;
+        }
+        
+        // Se não encontrar, usar nome original
+        return dataContext.name;
+    });
+
+    polygonSeries.mapPolygons.template.events.on("click", function(ev) {
+        var data = ev.target.dataItem.dataContext;
+        var name =  am5geodata_lang_PT[data.id] || data.name;
+        window.open(`/pages/${removeAccent(name).toLowerCase().replaceAll(" ", "")}.html`, "_self") 
+    });
+    polygonSeries.mapPolygons.template.setAll({
+    tooltipText: "{name}",
+    toggleKey: "active",
+    interactive: true,
+    cursorOverStyle: "pointer"
+    });
+
+    polygonSeries.mapPolygons.template.states.create("hover", {
+    fill: root.interfaceColors.get("primaryButtonHover"),
+    });
+
+    polygonSeries.mapPolygons.template.states.create("active", {
+    fill: root.interfaceColors.get("primaryButtonHover")
+    });
+
+    // Add zoom control
+    // https://www.amcharts.com/docs/v5/charts/map-chart/map-pan-zoom/#Zoom_control
+    var zoomControl = chart.set("zoomControl", am5map.ZoomControl.new(root, {}));
+    zoomControl.homeButton.set("visible", true);
+
+    // Make stuff animate on load
+    chart.appear(1000, 100);
+
+}); // end am5.ready()
+
+
